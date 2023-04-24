ad2ae926814858e0a30fef48ee784ed357f9b8de
Teo <teolampos@gmail.com>
2023-04-24 20:12:23 +0300

    deploy commit

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ server.post("/login", async (req, res) => {
 server.delete("/logout", async (req, res) => {
   try {
     res.clearCookie("ACCESS", { sameSite: "none" });
-    res.redirect("https://spool.onrender.com");
+    res.status(200).json({ success: true });
   } catch (err) {
     res.status(500).json({ success: false });
   }
