a1d3b66fdc8ec81cd0bddc2565e1fa94e1706b1a
Teo <teolampos@gmail.com>
2023-05-04 07:48:14 +0300

    deploy commit

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ server.post("/register", async (req, res) => {
     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
     res.cookie("ACCESS", token, {
       httpOnly: true,
-      secure: "none",
+      secure: "",
       sameSite: "none",
       maxAge: 24 * 60 * 1000, // COOKIE EXPIRES IN ONE DAY FROM THE MOMENT OF CREATION
     });
